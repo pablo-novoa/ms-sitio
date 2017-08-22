2b2e2b2c38cfc7b4bc47e2f569d8a8d0968c9c5d
pablo-novoa <novoanpablo@gmail.com>
2017-08-22 04:34:04 -0300

    init

---
+++ wp-content/themes/vantage-child/Widgets/Init.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace Widgets;
+
+/**
+* Widgets init class
+*/
+class Init
+{
+	
+	function __construct() {}
+
+	public static function register(){
+		
+	}
+}
---
+++ wp-content/themes/vantage-child/functions.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * AUTOLOADER
+ */
+spl_autoload_register(function ($className) {
+    $fullpath = dirname(__file__).'/'.str_replace('\\', '/', $className).'.php';
+    if(file_exists($fullpath)) require_once($fullpath);
+});
+
+
+// init widgets
+Widgets\Init::register();
+
+// enqueues
+add_action( 'wp_enqueue_scripts', 'ms_enqueue_styles' );
+function ms_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+
+//svg format to media library
+add_filter('upload_mimes', 'cc_mime_types');
+if (!function_exists('cc_mime_types')) {
+function cc_mime_types($mimes) {
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
+}}
+
+// Theme custom settings
+add_action('customize_register', 'ms_theme_settings');
+function ms_theme_settings($wp_customize){
+   //Redes
+  $wp_customize->add_section('ms_social_settings', array(
+    'title'          => 'MS - Redes Sociales'
+   ));
+  $wp_customize->add_setting('facebook_setting', array(
+   'default'        => '',
+   ));
+  $wp_customize->add_control('facebook_setting', array(
+   'label'   => 'Facebook',
+   'section' => 'ms_social_settings',
+   'type'    => 'url',
+  ));
+  $wp_customize->add_setting('instagram_setting', array(
+   'default'        => '',
+   ));
+  
+
+}
